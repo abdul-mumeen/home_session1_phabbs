da4daa723e97b632ae56d7db02f92b709ced2847
phabbs <olasodeadeyemi@gmail.com>
2017-01-06 23:49:37 +0100

    This is to update the changes made

--- minmax/findMinMaxTests.js
+++ minmax/findMinMaxTests.js
@@ -1,20 +1,40 @@
 (function() {
   'use strict';
+  var chai = require('chai');
+  var expect = chai.expect;
+
+  var myApp = require("../minmax/findMinMax.js");
 
   describe('Min-Max Numbers in a List: ', function () {
 
     describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
 
       it('should return [1,4] for [1, 2, 3 , 4]', function () {
-        expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+        expect(myApp.findMinMax([1, 2, 3, 4])) == [1, 4];
+      });
+
+      it('should return [-4,-1] for [-1, -2, -3 , -4]', function () {
+        expect(myApp.findMinMax([-1, -2, -3, -4])) == [-4,-1];
+      });
+
+      it('should return [1] for [1]', function () {
+        expect(myApp.findMinMax([1])) == [1];
+      });
+
+      it('should return "Empty Array" for []', function () {
+        expect(myApp.findMinMax([])) == "Empty Array";
       });
 
       it('should return [4, 6] for [6, 4]', function () {
-        expect(findMinMax([6, 4])).toEqual([4, 6]);
+        expect(myApp.findMinMax([6, 4])) == [4, 6];
       });
 
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
-        expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+        expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])) == [2, 78];
+      });
+
+       it('should return [8,9] for [8, 8, 8, 8, 8, 8, 8, 8, 9]', function () {
+        expect(myApp.findMinMax([8, 8, 8, 8, 8, 8, 8, 8, 9])) == [8, 9];
       });
 
     });
@@ -22,7 +42,11 @@
     describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
 
       it('should return [4] for [4, 4, 4, 4]', function () {
-        expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+        expect(myApp.findMinMax([4, 4, 4, 4])) == [4];
+      });
+
+      it('should return [8] for [8, 8, 8, 8, 8, 8, 8, 8]', function () {
+        expect(myApp.findMinMax([8, 8, 8, 8, 8, 8, 8, 8])) == [8];
       });
 
     });
